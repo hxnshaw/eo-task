57bb78eaa86b9c5f7fa14254a694c1e98b0eabed
hxnshaw <eghosasere07@gmail.com>
2022-10-30 22:15:01 +0100

    first commit

---
+++ app.js
@@ -0,0 +1,17 @@
+const express = require("express");
+const app = express();
+
+const PORT = 1000;
+
+app.listen(PORT, () => {
+  console.log(`Server is live on ${PORT}`);
+});
+
+app.get("/", (req, res) => {
+  res.json({
+    slackUsername: "Henshaw",
+    backend: true,
+    age: 25,
+    bio: "Hello World, my name is Eghosasere Osaghae, I am a junior backend developer, currently registered to the HNG internship. My hobbies include playing and watching football, playing video games, reading books and watching movies.",
+  });
+});
