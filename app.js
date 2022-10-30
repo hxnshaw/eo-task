9b26abe310310b66843ac652efb4055924511ce1
hxnshaw <eghosasere07@gmail.com>
2022-10-30 22:38:07 +0100

    bug fix

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const app = express();
 
-const PORT = 1000;
+const PORT = process.env.PORT || 1000;
 
 app.listen(PORT, () => {
   console.log(`Server is live on ${PORT}`);
